ceac16b2f1906c2f103924c63c70e3bee4032aad
Ryan Hou <rhou7873@gmail.com>
2022-07-06 00:37:30 -0400

    working on db

--- db_handler.py
+++ db_handler.py
@@ -1,5 +1,24 @@
+from datetime import datetime
+import discord
 from pymongo import MongoClient
 
 class DbHandler:
-    def __init__(self):
-        self.__db_client = MongoClient()
+    def __init__(self, db_conn_string: str) -> None:
+        self.__db = MongoClient(db_conn_string).general_walarus
+    
+    # Returns true if new document added
+    def log(self, server: discord.Guild) -> bool:
+        connected_servers = self.__db.connected_servers
+        server_data = {
+            "_id": server.id,
+            "name": str(server.name),
+            "description": str(server.description),
+            "icon_url": str(server.icon_url),
+            "creation_at": server.created_at,
+            "last_updated": datetime.now()
+        }
+        return self.__db.connected_servers.update_one({"_id": server.id}, 
+                                                      {"$set": server_data},
+                                                      upsert = True).upserted_id != None
+        
+        
