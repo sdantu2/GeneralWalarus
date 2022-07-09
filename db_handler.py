b1e55fb1a24014d21cec8b9990a3e66622539aa7
Ryan Hou <rhou7873@gmail.com>
2022-07-09 01:05:31 -0400

    working on vc clipping feature

--- db_handler.py
+++ db_handler.py
@@ -1,13 +1,14 @@
 from datetime import datetime
 import discord
+from gridfs import Collection
 from pymongo import MongoClient
 
 class DbHandler:
     def __init__(self, db_conn_string: str) -> None:
         self.__db = MongoClient(db_conn_string).general_walarus
+        self.__log = lambda discord_server, collection, data: collection.update_one({"_id": discord_server.id}, {"$set": data}, upsert = True).upserted_id != None
     
-    def log_user_stat(self, discord_server: discord.Guild, user: discord.User, target_stat: str,
-                      inc = 1) -> bool:
+    def log_user_stat(self, discord_server: discord.Guild, user: discord.User, target_stat: str, inc = 1) -> bool:
         user_stats = self.__db.user_stats
         stats = ["mentioned", "sent_messages", "vc_time"]
         stats_data = {}
@@ -33,25 +34,19 @@ class DbHandler:
     
     def log_server(self, discord_server: discord.Guild) -> bool:
         connected_servers = self.__db.connected_servers
+        icon_exists = bool(discord_server.icon)
         server_data = {
             "_id": discord_server.id,
             "name": str(discord_server.name),
             "description": str(discord_server.description),
-            "icon_url": str(discord_server.icon_url),
+            "icon_url": str(discord_server.icon.url) if icon_exists else "",
             "creation_at": discord_server.created_at,
             "last_updated": datetime.now()
         }
-        return self.__log(discord_server, "connected_servers", server_data)
+        return self.__log(discord_server, connected_servers, server_data)
     
-    # Returns true if new document added
-    def __log(self, discord_server: discord.Guild, collection_name: str, data: dict) -> bool:
-        collection = self.__db.get_collection(collection_name)
-        return collection.update_one({
-                                        "_id": discord_server.id
-                                    }, 
-                                    {
-                                        "$set": data
-                                    }, 
-                                     upsert = True).upserted_id != None
+    def remove_discord_server(self, discord_server: discord.Guild) -> bool:
+        connected_servers = self.__db.connected_servers
+        return connected_servers.delete_one({"_id": discord_server.id}).deleted_count == 1
         
         
---
+++ test.py
@@ -0,0 +1,56 @@
+import discord
+from discord.ext import commands
+import os
+from dotenv import load_dotenv
+from discord.sinks import WaveSink, Sink
+
+intents = discord.Intents.default()
+intents.message_content = True
+bot = commands.Bot(command_prefix="!", intents=intents)
+load_dotenv()
+connections = {}
+
+@bot.event
+async def on_ready() -> None:
+    print("test walarus is up and ready")
+
+@bot.command(name="herro")
+async def herro(ctx: commands.Context) -> None:
+    await ctx.send("Hello there!")
+
+@bot.command()
+async def record(ctx):  # If you're using commands.Bot, this will also work.
+    voice = ctx.author.voice
+
+    if not voice:
+        await ctx.respond("You aren't in a voice channel!")
+
+    vc = await voice.channel.connect()  # Connect to the voice channel the author is in.
+    connections.update({ctx.guild.id: vc})  # Updating the cache with the guild and channel.
+
+    vc.start_recording(
+        WaveSink(),  # The sink type to use.
+        once_done,  # What to do once done.
+        ctx.channel  # The channel to disconnect from.
+    )
+    await ctx.send("Started recording!")
+
+async def once_done(sink: Sink, channel: discord.TextChannel, *args):  # Our voice client already passes these in.
+    recorded_users = [  # A list of recorded users
+        f"<@{user_id}>"
+        for user_id, audio in sink.audio_data.items()
+    ]
+    files = [discord.File(audio.file, f"{user_id}.{sink.encoding}") for user_id, audio in sink.audio_data.items()]  # List down the files.
+    await channel.send(f"finished recording audio for: {', '.join(recorded_users)}.", files=files)  # Send a message with the accumulated files.
+
+@bot.command(name="stop")
+async def stop_recording(ctx: commands.Context):
+    if ctx.guild.id in connections:  # Check if the guild is in the cache.
+        vc = connections[ctx.guild.id]
+        vc.stop_recording()  # Stop recording, and call the callback (once_done).
+        del connections[ctx.guild.id]  # Remove the guild from the cache.
+    else:
+        await ctx.send("I am currently not recording here.")  # Respond with this if we aren't recording.
+
+print(os.getenv("BOT_TOKEN"))
+bot.run(os.getenv("BOT_TOKEN"))
