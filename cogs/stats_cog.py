b4a5238d65abd68d1114b8aeeff0b460c5ca9676
Ryan Hou <rhou7873@gmail.com>
2023-02-18 18:50:49 -0600

    redesigned time unit logic

--- cogs/stats_cog.py
+++ cogs/stats_cog.py
@@ -4,6 +4,20 @@ from discord.ext import commands
 import database as db
 from typing import cast
 
+class _TimeSpan():
+    def __init__(self, seconds: int):
+        self.days = seconds // 86400
+        self.days_unit = "day" if self.days == 1 else "days"
+        seconds -= self.days * 86400
+        self.hours = seconds // 3600
+        self.hours_unit = "hour" if self.hours == 1 else "hours"
+        seconds -= self.hours * 3600
+        self.minutes = seconds // 60
+        self.minutes_unit = "minute" if self.minutes == 1 else "minutes"
+        seconds -= self.minutes * 60
+        self.seconds = seconds
+        self.seconds_unit = "second" if self.seconds == 1 else "seconds"
+        
 class StatisticsCog(Cog, name="Statistics"):
     """ Class containing General Walarus' statistics commands """
     
@@ -31,32 +45,23 @@ class StatisticsCog(Cog, name="Statistics"):
             if guild != None:
                 query = cast(dict, db.get_user_stats(guild, user.id, "time_in_vc"))
                 seconds = int(query["time_in_vc"])
-                days = seconds // 86400
-                seconds -= days * 86400
-                hours = seconds // 3600
-                seconds -= hours * 3600
-                minutes = seconds // 60
-                seconds -= minutes * 60
-                unit: dict = {
-                    days: "days" if days > 1 else "day",
-                    hours: "hours" if hours > 1 else "hour",
-                    minutes: "minutes" if minutes > 1 else "minute",
-                    seconds: "seconds" if seconds > 1 or seconds == 0 else "second"
-                }
+                time: _TimeSpan = _TimeSpan(seconds)
                 msg = f"You've spent" if user.id == ctx.author.id else f"{user.name} has spent"
-                if days > 0:
-                    msg += (f" {days} {unit[days]}, {hours} {unit[hours]}, {minutes} {unit[minutes]}, "
-                            f"and {seconds} {unit[seconds]} in voice channel")
-                elif hours > 0:
-                    msg += (f" {hours} {unit[hours]}, {minutes} {unit[minutes]}, and {seconds} " 
-                            f"{unit[seconds]} in voice channel")
-                elif minutes > 0:
-                    msg += (f" {minutes} {unit[minutes]}, and {seconds} {unit[seconds]} in "
-                            f"voice channel")
+                if time.days > 0:
+                    msg += (f" {time.days} {time.days_unit}, {time.hours} {time.hours_unit}, "
+                            f"{time.minutes} {time.minutes_unit}, and {time.seconds} "
+                            f"{time.seconds_unit} in voice channel")
+                elif time.hours > 0:
+                    msg += (f" {time.hours} {time.hours_unit}, {time.minutes} "
+                            f"{time.minutes_unit}, and {time.seconds} "
+                            f"{time.seconds_unit} in voice channel")
+                elif time.minutes > 0:
+                    msg += (f" {time.minutes} {time.minutes_unit}, and {time.seconds} "
+                            f"{time.seconds_unit} in voice channel")
                 else:
-                    msg += f" {seconds} {unit[seconds]} in voice channel"
+                    msg += f" {time.seconds} {time.seconds_unit} in voice channel"
                 await ctx.send(msg)
         except Exception as e:
             print(str(e))
             await ctx.send("I pooped my pants...try again")
-    
+
