47f8981104abbec949572877bb3db5bf52228bcb
Ryan Hou <rhou7873@gmail.com>
2023-05-10 20:24:50 -0400

    fixed timezone issues

--- cogs/miscellaneous_cog.py
+++ cogs/miscellaneous_cog.py
@@ -4,6 +4,7 @@ import database as db
 from datetime import datetime
 from globals import servers
 from models import Server, TimeSpan
+from pytz import timezone
 from utilities import now_time, timef
 from typing import cast
 
@@ -58,8 +59,8 @@ class MiscellaneousCog(Cog, name="Miscellaneous"):
     
     @commands.command(name="anjayunban")
     async def anjay_unban(self, ctx: commands.Context) -> None:
-        now: datetime = datetime.now()
-        datetime_unbanned: datetime = datetime(year=2023, month=6, day=4, hour=9, minute=8)
+        now: datetime = datetime.now(tz=timezone("US/Eastern"))
+        datetime_unbanned: datetime = datetime(year=2023, month=6, day=4, hour=21, minute=8)
         sec_until: int = (datetime_unbanned - now).total_seconds().__floor__()
 
         if sec_until <= 0:
