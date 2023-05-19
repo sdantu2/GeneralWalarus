51b222f9cd45866afc30f097b6b7a311d7bb8eb6
Ryan Hou <rhou7873@gmail.com>
2023-05-19 16:23:40 -0400

    each server has unique chat to archive and archive category

--- database/__init__.py
+++ database/__init__.py
@@ -1,3 +1,13 @@
-from .db_archive import get_next_archive_date, update_next_archive_date
-from .db_servers import log_server, remove_discord_server, get_rshuffle, get_ushuffle
-from .db_user_stats import inc_user_stat, update_user_stats, get_user_stat, get_user_stats, create_user
+from .db_archive import (get_next_archive_date, 
+                         update_next_archive_date)
+from .db_servers import (log_server, 
+                         remove_discord_server, 
+                         get_rshuffle, 
+                         get_ushuffle,
+                         get_archive_category,
+                         get_chat_to_archive)
+from .db_user_stats import (inc_user_stat, 
+                            update_user_stats, 
+                            get_user_stat, 
+                            get_user_stats, 
+                            create_user)
